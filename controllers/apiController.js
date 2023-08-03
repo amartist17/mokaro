19f7344e70d2411659d94313b4e8edb03726e6b7
Amar Singh <gujraal2006@gmail.com>
2023-08-03 14:22:23 +0530

    second

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,8 +1,26 @@
+const Invoice = require('../models/invoiceModel')
+
 exports.sendmail = async (req, res, next) => {
     try {
     //   hi
     } catch (err) {
       let message = "unable to send mail"
+      return res.render("error", {
+        status: 400,
+        message: message,
+      });
+    }
+  };
+  
+  exports.createInvoice = async (req, res, next) => {
+    try {
+    //   hi
+    console.log(req.body)
+    let newInvoice = await Invoice.create(
+      req.body
+    )
+    } catch (err) {
+      let message = "unable to generate invoice"
       return res.render("error", {
         status: 400,
         message: message,
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -8,7 +8,8 @@ const router = express.Router({ mergeParams: true });
 
 router.post("/login", authController.login);
 router.post("/signup", authController.signup);
-router.post("/sendmail", apiController.sendmail);
+router.post("/sendmail",authController.protect, apiController.sendmail);
+router.post("/create-invoice",authController.protect, apiController.createInvoice);
 
 
 module.exports = router;
